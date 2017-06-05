d98a6072f93665d19b4b6c664c4acdeb746bcaff
shieldhub1 <danesh@shieldhub.in>
2017-06-05 17:40:11 +0530

    Create graylog.sh

---
+++ graylog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Fist shell script for GrayLog Server
+sudo apt-get install apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen -y
+sudo apt-get install mongodb-server -y
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
+sudo echo "cluster.name: graylog" >> /etc/elasticsearch/elasticsearch.yml
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch.service
+sudo systemctl restart elasticsearch.service
